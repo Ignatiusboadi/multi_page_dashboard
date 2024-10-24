5ee93162a18546b4dd46f464b74ab6ee9137bd32
Ignatius Boadi <ignatiusboadi@gmail.com>
2024-10-24 16:59:43 +0000

    added code to index file which control the navigation to other pages

---
+++ index.py
@@ -0,0 +1,26 @@
+from app import app
+from dash import dcc, html, ctx, callback
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content'),
+    dcc.Store(id='token', data=None),
+    dbc.Row(children=[
+        dbc.Col(html.H5([html.I(className='fa fa-copyright'), ' Group 1 2024'], style={'padding-top': '5px'}),
+                width={"size": 2, 'offset': 10})])])
+
+
+@app.callback(Output('page-content', 'children'),
+              Input('url', 'pathname'),
+              Input('token', 'data'))
+def display_page(pathname, token):
+    if pathname == '/' or not token:
+        return
+    else:
+        return '404: Page not found'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8050)
